c6e46dd00a1b7006bf3ba2c2084cd94f8e918b17
BOB AI <66891366+akdnsnd@users.noreply.github.com>
2026-03-10 17:46:06 +0000

    fix bug v1.0.2

--- bob.py
+++ bob.py
@@ -3,31 +3,30 @@
 
 def iniciar_bob():
     # Esta menssagem aparece somente quando o Bob e chamado
-    print('Iniciando Sistemas...')
-    print('Salve chefe, o que manda??')
+    print ("Iniciando Sistemas...")
+    print ("Salve chefe, o que manda??")
     # A variavel 'comando'  guarda o comando que voce digita
     # O .lower() deixa as letras minusculas 
     while True:
-        comando = input('\n Comando: ').lower()
+        comando = input ("\n Comando: ").lower()
 
         # --- CEREBRO DO BOB --- 
         # Aqui ele verifica e responde
         # break acaba com o loop
-        if 'ola' in comando or 'oi' in comando:
-            print('Bob: Eae tudo bem?')
+        if "ola" in comando or "oi" in comando:
+            print ("Bob: Eae tudo bem?")
 
-        elif 'seu nome' in comando:
-            print('Bob: Eu sou o Bob e estou em treinamento')
+        elif "seu nome" in comando:
+            print ("Bob: Eu sou o Bob e estou em treinamento")
 
-        elif 'desligar' in comando or 'sair' in comando:
-            print('Bob: Valeu, fui!')
+        elif "desligar" in comando or "sair" in comando:
+            print ("Bob: Valeu, fui!")
             break
 
         else:
             # Se ele nao reconhecer o comando ele da uma resposta padrao
-            print('Bob: Pode repetir chefe?')
+            print ("Bob: Pode repetir chefe?")
 
 # Esse comando e oque faz para o Bob entender e processar
-if __name__ == '__main__':
-    iniciar_bob
+iniciar_bob()
 
